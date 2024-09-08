f850cd71eb6e2c2b44d7af7e05936de0f53d0cef
Nicolas Buszczak <nico_947quil@hotmail.com>
2024-09-08 03:04:05 -0300

    Sketch + graph

--- frontend/src/components/CountryContainer/CountryContainer.jsx
+++ frontend/src/components/CountryContainer/CountryContainer.jsx
@@ -1,20 +1,24 @@
 import { useEffect, useState } from "react";
+import { Chart as ChartJS } from "chart.js/auto";
+import { Line } from "react-chartjs-2";
 const API_URL = import.meta.env.VITE_API_URL;
 
 const CountryContainer = ({ country, handleClick }) => {
   const [countryInfo, setCountryInfo] = useState(null);
   useEffect(() => {
-    console.log('hellos');
-    
+    console.log("hellos");
+
     fetch(`${API_URL}/${country.countryCode}`)
       .then((response) => response.json())
       .then((data) => setCountryInfo(data));
-    return () => {setCountryInfo(null)};
+    return () => {
+      setCountryInfo(null);
+    };
   }, [country]);
 
   return (
     <div className="flex flex-col items-center">
-      <button onClick={handleClick} className="self-end mr-10 mt-10">
+      <button onClick={handleClick} className="self-end mr-10 mt-10 border px-2">
         Back
       </button>
       {country.name}
@@ -28,7 +32,25 @@ const CountryContainer = ({ country, handleClick }) => {
             {borderCountry.country}
           </button>
         ))}
-      </div>
+      </div >
+      {countryInfo ? (
+        <div className="w-2/3 border p-4 my-4">
+        <Line
+          data={{
+            labels: countryInfo.countryPopulation.map((data) => data.year),
+            datasets: [
+              {
+                label: "Population by year",
+                data: countryInfo.countryPopulation.map((data) => data.value),
+                fill: false,
+                borderColor: "rgb(75, 192, 192)",
+                tension: 0.1,
+              },
+            ],
+          }}
+        />
+        </div>
+      ) : null}
     </div>
   );
 };
